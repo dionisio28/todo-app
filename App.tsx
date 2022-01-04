bb84bc68052f5580a77a438503a2f44a08eaeabb
Dionisio <dionisio@malteso.com>
2022-01-04 19:32:51 -0400

    Remove Redux Dev Tools

--- App.tsx
+++ App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import AppContainer from "./src/components/app-container";
 import Routes from "./src/routes";
-import { connectToDevTools } from "react-devtools-core";
 
-if (__DEV__) {
-  connectToDevTools({
-    host: "localhost",
-    port: 8097,
-  });
-}
 export default function App() {
   return (
     <AppContainer>
